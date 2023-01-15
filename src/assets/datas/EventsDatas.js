eaa06406163fbd4d697811fa84c006b7507d48ef
Syam Kumar <yadavallisyam1@gmail.com>
2023-01-15 14:19:33 +0530

    sidenav event card css

--- src/assets/datas/EventsDatas.js
+++ src/assets/datas/EventsDatas.js
@@ -2,52 +2,156 @@ import file1 from '../images/file1.png'
 
 const events = [
     {
-        id:1,
-        title:"Chess",
-        details:"Sint tempore libero veritatis ad repellat cum sapiente eveniet voluptatem, pariatur temporibus.",
-        date:"28-02-23",
-        time:"2 P.M.",
-        img:file1
+        id: 1,
+        title: "Treasure Hunt",
+        details: "Find the blackbox lost in space-time by decoding cryptic signals",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
     },
     {
-        id:2,
-        title:"Chess",
-        details:"Sint tempore libero veritatis ad repellat cum sapiente eveniet voluptatem, pariatur temporibus.",
-        date:"28-02-23",
-        time:"2 P.M.",
-        img:file1
+        id: 18,
+        title: "Valorant Tournament",
+        details: "A Gaming Competition for the Valorant",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
     },
     {
-        id:3,
-        title:"Chess",
-        details:"Sint tempore libero veritatis ad repellat cum sapiente eveniet voluptatem, pariatur temporibus.",
-        date:"28-02-23",
-        time:"2 P.M.",
-        img:file1
+        id: 19,
+        title: "Speedcubing",
+        details: "Rubik's Cube solving Competition",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
     },
     {
-        id:4,
-        title:"Chess",
-        details:"Sint tempore libero veritatis ad repellat cum sapiente eveniet voluptatem, pariatur temporibus.",
-        date:"28-02-23",
-        time:"2 P.M.",
-        img:file1
+        id: 2,
+        title: "Robotrek",
+        details: "Robot car race",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
     },
     {
-        id:5,
-        title:"Chess",
-        details:"Sint tempore libero veritatis ad repellat cum sapiente eveniet voluptatem, pariatur temporibus.",
-        date:"28-02-23",
-        time:"2 P.M.",
-        img:file1
+        id: 3,
+        title: "VR Show",
+        details: "Immerse yourself in the unreality.",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
     },
     {
-        id:6,
-        title:"Chess",
-        details:"Sint tempore libero veritatis ad repellat cum sapiente eveniet voluptatem, pariatur temporibus.",
-        date:"28-02-23",
-        time:"2 P.M.",
-        img:file1
+        id: 4,
+        title: "Shipwreck",
+        details: "when your ship wrecks and you fall into the shark tank.",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 5,
+        title: "Best out of waste",
+        details: "Creativity is making marvelous out of the Discarded.",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 6,
+        title: "Blind Coding",
+        details: "Why look at the screen when you can talk to it in Binary",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 7,
+        title: "CodeTag",
+        details: "Explain the Coding problem statement only through Pictures",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 8,
+        title: "Hydrolift",
+        details: "Make Hydraulic Gadgets using Pascal's Law",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 9,
+        title: "Animatrix",
+        details: "Animated Video Making Competition",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 10,
+        title: "Click'O'Vartan",
+        details: "For the People who see the world through the camera",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 11,
+        title: "Blind Coding",
+        details: "Why look at the screen when you can talk to it in Binary",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 12,
+        title: "Scavenger Hunt",
+        details: "A tresure hunt for space scavengers",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 13,
+        title: "Technomic Quotient",
+        details: "A Quiz on Technology",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 14,
+        title: "October sky",
+        details: "Launch your own rockets here",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 15,
+        title: "ECOPOLIS",
+        details: "For the architects in the crowd to plan a city",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 16,
+        title: "Beg-borrow-steal",
+        details: "In simple words, you have to either Beg, Borrow or steal",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
+    },
+    {
+        id: 17,
+        title: "Jenga",
+        details: "Life is like a game of Jenga",
+        date: "28-02-23",
+        time: "2 P.M.",
+        img: file1
     },
 ];
 
--- src/components/Cards/EventCard.js
+++ src/components/Cards/EventCard.js
@@ -1,31 +1,33 @@
 import React from 'react'
 import styles from './EventCard.module.css'
 
-const EventCard = ({img,title,date,time,details}) => {
-  return (
-    <>
-    <div className={styles.eventcardcard}>
-        <img class={styles.eventcardimage} src={img} alt="Event Poster"></img>
-        <div className={styles.eventcardcardtitle} style={{}}>
-            {title}
-        </div>
-        <div className={styles.eventcardline}>
-        </div>
-        <div className={styles.eventcardcarddetails}>
-            {details}
-        </div>
-        <div className={styles.eventcardcarddetails}>
-            Date : {date}
-        </div>
-        <div className={styles.eventcardcarddetails}>
-            Time : {time}
-        </div>
-        <div className={styles.aavartanbutton}>
-            <button>Register</button>
-        </div>
-    </div>
-    </>
-  )
+const EventCard = ({ img, title, date, time, details }) => {
+    return (
+        <>
+            <div className={styles.eventcardcard}>
+                <img class={styles.eventcardimage} src={img} alt="Event Poster"></img>
+                <div className={styles.eventdetails}>
+                    <div className={styles.eventcardcardtitle} style={{}}>
+                        {title}
+                    </div>
+                    <div className={styles.eventcardline}>
+                    </div>
+                    <div className={styles.eventcardcarddetails}>
+                        {details}
+                    </div>
+                    <div className={styles.eventcardcarddetails}>
+                        Date : {date}
+                    </div>
+                    <div className={styles.eventcardcarddetails}>
+                        Time : {time}
+                    </div>
+                    <div className={styles.aavartanbutton}>
+                        <button>Register</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default EventCard

59312b3437c16f372009ca4bcaafd9c7a907db9e
Syam Kumar <yadavallisyam1@gmail.com>
2023-01-27 16:16:30 +0530

    hero, spons minor ui

--- src/components/Sponsors/TitleSponsor/TitleSponsor.jsx
+++ src/components/Sponsors/TitleSponsor/TitleSponsor.jsx
@@ -1,13 +1,13 @@
 
 import React from "react";
 import style from "../TitleSponsor/TitleSponsor.module.css";
-function TitleSponsor(){
-    return(
+function TitleSponsor() {
+    return (
         <div className={style.titlecontainer}>
-                <div className={style.titlecard}>
-                <cite className={style.sub}>&mdash;Aavartan'19 Title Sponsor</cite>
-                </div>
+            <div className={style.titlecard}>
+                <cite className={style.sub}>Aavartan'19 Title Sponsor</cite>
             </div>
+        </div>
     );
 }
 

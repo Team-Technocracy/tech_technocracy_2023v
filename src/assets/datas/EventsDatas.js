5054f5ad5acfab202f127884fc377add19186989
D Anuj Kumar <anuj.as828@gmail.com>
2023-02-01 02:08:45 +0530

    images added

--- src/assets/datas/EventsDatas.js
+++ src/assets/datas/EventsDatas.js
@@ -1,9 +1,118 @@
-import file1 from '../images/file1.png'
+import file1 from '../images/eventImages/file1.webp'
+import file2 from '../images/eventImages/file2.webp'
+import file3 from '../images/eventImages/file.webp'
+import file4 from '../images/eventImages/file4.webp'
+import file5 from '../images/eventImages/file5.webp'
+import file6 from '../images/eventImages/file6.webp'
+import file7 from '../images/eventImages/file7.webp'
+import file8 from '../images/eventImages/file.webp'
+import file9 from '../images/eventImages/file9.webp'
+import file10 from '../images/eventImages/file.webp'
+import file11 from '../images/eventImages/file.webp'
+import file12 from '../images/eventImages/file12.webp'
+import file13 from '../images/eventImages/file13.webp'
 
 const events = [
-
     {
         id: 1,
+        title: "Animatrix",
+        details: "Bring your imagination to life. Animate your ideas and let them come alive in our competition. Unleash the power of animation and join us! Your creativity knows no bounds.",
+        date: "04-02-23",
+        time: "3 P.M.",
+        img: file10,
+        teamSize: 1,
+        teamMin: 1
+    },
+    {
+        id: 2,
+        title: "Best out of waste",
+        details: "“Recycle it all, No matter how small! “ with this intent unveil your creativity, artistry, proficiency and dexterity, spawning marvelous adornments from discarded scrap. ",
+        date: "04-02-23",
+        time: "11 A.M.",
+        img: file6,
+        teamSize: 3,
+        teamMin: 2
+    },
+    {
+        id: 3,
+        title: "Blind Coding",
+        details: " Even without sight, there is still a vision. Hold on tight for this electrifying coding escapade to appraise your skills, logic, and limits.",
+        date: "04-02-23",
+        time: "9 A.M.",
+        img: file7,
+        teamSize: 1,
+        teamMin: 1
+    },
+    {
+        id: 4,
+        title: "Circuitrix",
+        details: " A circuit-related game that will pique your interest. Get ready to step into the circuit realm.",
+        date: "04-02-23",
+        time: "10 A.M.",
+        img: file2,
+        teamSize: 0,
+        condition: ''
+    },
+    {
+        id: 5,
+        title: "CodeTag",
+        details: "Get yourself prepared to scrutinize, perceive, and decode the problem statement from images. Make no provision to compete with the finest intellects of coding. ",
+        date: "05-02-23",
+        time: "9 A.M.",
+        img: file8,
+        teamSize: 3,
+        teamMin: 3
+    },
+    {
+        id: 6,
+        title: "ECOPOLIS",
+        details: "Plan! Execute! Deliver! in that order. Join us at the city planning event. Put your wit to work and design an urban planning layout of an upcoming city for a chance to get exciting rewards.",
+        date: "04-02-23",
+        time: "2 P.M.",
+        img: file12,
+        teamSize: 3,
+        teamMin: 1
+    },
+    {
+        id: 7,
+        title: "Hydrolift",
+        details: "Experience the extraordinary at our one-of-a-kind event.This is that one time in life that pressure's good for you .Apply that pressure on the correct area and may the force be with you.",
+        date: "05-02-23",
+        time: "3 P.M.",
+        img: file9,
+        teamSize: 3,
+        teamMin: 2
+    },
+    {
+        id: 8,
+        title: "Robotrek",
+        details: "Slow and steady wins the race”, but is it true for the robots as well? Find out in this extraordinary racing event where the self made robot cars will compete to accomplish the race. ",
+        date: "04-02-23",
+        time: "3 P.M.",
+        img: file3,
+        teamSize: 3
+    },
+    {
+        id: 9,
+        title: "Scavenger Hunt",
+        details: "A box with no hinges, key or lid, yet has a golden treasure hidden in it. Solve this riddle, and you may be the one to uncover the exceptional treasure within.",
+        date: "05-02-23",
+        time: "2 P.M.",
+        img: file11,
+        teamSize: 4,
+        teamMin: 4
+    },
+    {
+        id: 10,
+        title: "Shipwreck",
+        details: " A fun-driving event where one has to portray the personality of an eminent person, and save oneself from a sinking ship, beating out others.        ",
+        date: "05-02-23",
+        time: "11 A.M.",
+        img: file5,
+        teamSize: 5
+    },
+    {
+        id: 11,
         title: "Treasure Hunt",
         details: "Find the blackbox lost in space-time by decoding cryptic signals",
         date: "04-02-23",
@@ -11,126 +120,26 @@ const events = [
         img: file1,
         teamSize: 5,
     },
-    {
-        id: 2,
-        title: "Circuitrix",
-        details: " A circuit-related game that will pique your interest. Get ready to step into the circuit realm.",
-        date: "04-02-23",
-        time: "10 A.M.",
-        img: file1,
-        teamSize: 0,
-        condition: ''
-    },
-    {
-        id: 3,
-        title: "Robotrek",
-        details: "Slow and steady wins the race”, but is it true for the robots as well? Find out in this extraordinary racing event where the self made robot cars will compete to accomplish the race. ",
-        date: "04-02-23",
-        time: "3 P.M.",
-        img: file1,
-        teamSize: 3
-    },
-    {
-        id: 4,
-        title: "VR Show",
-        details: " Get yourself ready for a thrilling chance to explore the world of Virtual Reality through VR headsets recreating the images in our mind.",
-        date: "05-02-23",
-        time: "9 A.M.",
-        img: file1,
-        teamSize: 1
-    },
-    {
-        id: 5,
-        title: "Shipwreck",
-        details: " A fun-driving event where one has to portray the personality of an eminent person, and save oneself from a sinking ship, beating out others.        ",
-        date: "05-02-23",
-        time: "11 A.M.",
-        img: file1,
-        teamSize: 5
-    },
-    {
-        id: 6,
-        title: "Best out of waste",
-        details: "“Recycle it all, No matter how small! “ with this intent unveil your creativity, artistry, proficiency and dexterity, spawning marvelous adornments from discarded scrap. ",
-        date: "04-02-23",
-        time: "11 A.M.",
-        img: file1,
-        teamSize: 3,
-        teamMin: 2
-    },
-    {
-        id: 7,
-        title: "Blind Coding",
-        details: " Even without sight, there is still a vision. Hold on tight for this electrifying coding escapade to appraise your skills, logic, and limits.",
-        date: "04-02-23",
-        time: "9 A.M.",
-        img: file1,
-        teamSize: 1,
-        teamMin: 1
-    },
-    {
-        id: 8,
-        title: "CodeTag",
-        details: "Get yourself prepared to scrutinize, perceive, and decode the problem statement from images. Make no provision to compete with the finest intellects of coding. ",
-        date: "05-02-23",
-        time: "9 A.M.",
-        img: file1,
-        teamSize: 3,
-        teamMin: 3
-    },
-    {
-        id: 9,
-        title: "Hydrolift",
-        details: "Experience the extraordinary at our one-of-a-kind event.This is that one time in life that pressure's good for you .Apply that pressure on the correct area and may the force be with you.",
-        date: "05-02-23",
-        time: "3 P.M.",
-        img: file1,
-        teamSize: 3,
-        teamMin: 2
-    },
-    {
-        id: 10,
-        title: "Animatrix",
-        details: "Bring your imagination to life. Animate your ideas and let them come alive in our competition. Unleash the power of animation and join us! Your creativity knows no bounds.",
-        date: "04-02-23",
-        time: "3 P.M.",
-        img: file1,
-        teamSize: 1,
-        teamMin: 1
-    },
-    {
-        id: 11,
-        title: "Scavenger Hunt",
-        details: "A box with no hinges, key or lid, yet has a golden treasure hidden in it. Solve this riddle, and you may be the one to uncover the exceptional treasure within.",
-        date: "05-02-23",
-        time: "2 P.M.",
-        img: file1,
-        teamSize: 4,
-        teamMin: 4
-    },
-
 
     {
         id: 12,
-        title: "ECOPOLIS",
-        details: "Plan! Execute! Deliver! in that order. Join us at the city planning event. Put your wit to work and design an urban planning layout of an upcoming city for a chance to get exciting rewards.",
-        date: "04-02-23",
-        time: "2 P.M.",
-        img: file1,
-        teamSize: 3,
-        teamMin: 1
-    },
-    {
-        id: 13,
         title: "Valorant Tournament",
         details: "Buckle Up! As easy as this event may seem ,the harder it is in actuality. Can you last everyone out in this 5v5 face-off ? ",
         date: "05-02-23",
         time: "9 A.M.",
-        img: file1,
+        img: file13,
         teamSize: 5,
         teamMin: 5
     },
- 
+    {
+        id: 13,
+        title: "VR Show",
+        details: " Get yourself ready for a thrilling chance to explore the world of Virtual Reality through VR headsets recreating the images in our mind.",
+        date: "05-02-23",
+        time: "9 A.M.",
+        img: file4,
+        teamSize: 1
+    },
 ];
 
 export default events;

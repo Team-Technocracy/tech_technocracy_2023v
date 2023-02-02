cb4affc660ebe69a740b52642cd1b6ab9a872dde
Rahul singh <rahulsingh19390@gmail.com>
2023-02-02 16:11:14 +0530

    Added google form For Technomic Quotient

---
+++ src/components/Forms/Registration/Gform.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Gform() {
+  return (
+    <div>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdVe65UmhXjjlxwcibPK7Zm_FGbpgON4p8iwk4DAtobV9O41w/viewform?embedded=true" width="100%" height="1541" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+  )
+}
+
+export default Gform

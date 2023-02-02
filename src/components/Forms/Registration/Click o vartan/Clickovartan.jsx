16a57c0dde3f1b180850978351d26ee65f8a7760
Rahul singh <rahulsingh19390@gmail.com>
2023-02-02 17:09:05 +0530

    Added Navlink and Details

--- src/components/Forms/Registration/Click o vartan/Clickovartan.jsx
+++ src/components/Forms/Registration/Click o vartan/Clickovartan.jsx
@@ -98,13 +98,55 @@ function Clickovartan() {
 				<div className={styles.goback}>
 						<NavLink to="/events"><img src={img} alt="" /></NavLink>
 					</div>
-					<div className={styles.description}>
-						{/* <div className={styles.event_image}></div> */}
-						<div>
-							<h3 className={styles.event_title}>Click 'O' Vartan</h3>
-							<p className={styles.event_description}> "A photograph is worth a thousand words." Capture the moment and it shall become undead. Hold your cameras tight! and get ready to capture the entire event in your lens. May the best Capture win!
-							</p>
-						</div>
+						<div className={styles.description}>
+							{/* <div className={styles.event_image}></div> */}
+							<div>
+								<h3 className={styles.event_title}>Click 'O' Vartan</h3>
+								<p className={styles.event_description}>"A photograph is worth a thousand words." Capture the moment and it shall become undead. Hold your cameras tight! and get ready to capture the entire event in your lens. May the best Capture win!</p>
+								<p className={styles.event_more}><b>DESCRIPTION: </b>This Aavartan we are organising an interesting photography competition for them who are crazy for selfies and experts in clicking the moments. During Aavartan there will be a number of events and attractions, we are organising so you just click your photo of precious and joyful moments and fill the form given below. Pictures may be of any event or attraction. And we will select the top three amazing pictures and give goodies to them.  </p>
+								<p className={styles.event_materials}>
+									<b>Judging criteria:</b>
+									<ul>
+										<br></br>
+										<li>- Participants have to submit their photos as well as share their entry on their stories tagging @nitrr.aavatan and @clickclubnitrr with the hashtag #Aavartan’22-23 and #clickclubnitrr. </li>
+										<br></br>
+										<li>- Each participant should submit only one photo. Participants can also upload photos that they had clicked before the start date of the competition. </li>
+										<br></br>
+										<li>- Watermarked photos shall not be considered.  </li>
+										<br></br>
+										<li>- Entries within the given time period shall only be considered.  </li>
+										<br></br>
+										<li>- The authority of final decision regarding any case of conflicts shall lie with Click Club only.  </li>
+										<br></br>
+										<li>- Judgement shall be done on the basis of composition, colours and overall aesthetics of the image.   </li>
+										<br></br>
+										<li>- Basic photo editing is allowed. Photo manipulation is not allowed. </li>
+										<br></br>
+										<li>- Images captured from cameras as well as mobiles are accepted.  </li>
+										<br></br>
+										<li>- The authenticity of the image should be the sole responsibility of the photographer submitting, NIT Raipur holds no responsibility in case of any discrepancy. </li>
+										<br></br>
+										<li>- You will be asked for the original RAW/JPEG picture before the declaration of final results.  </li>
+										<br></br>
+										{/* <li>- Some of the required waste material can be provided to the team. Though, the participants need to bring their own Waste Material. But use of partial, whole or readymade models and any other unfair means will directly lead to disqualification.</li> */}
+										{/* <li>- Each member of the team must contain the identity card of his/her respected institute.</li> */}
+									</ul>
+								</p>
+								{/* <p className={styles.event_materials}>
+								<b>EVENT JUSTIFICATION : </b>
+								<ul>
+									<li>- Tech skills</li>
+									<li>- Creative Thinking</li>
+									<li>- Presence of mind</li>
+								</ul>
+							</p> */}
+								{/* <p className={styles.event_team}><b>Requirements: </b>○ waste products, Number of members - 2, Volunteers – 4-5</p> */}
+								<p className={styles.event_procedure}><b>PRIZES : </b>Vouchers and Goodies</p>
+								<p className={styles.event_location}><b>LOCATION : </b>Entire NIT's Campus</p>
+								<p className={styles.event_time}><b>Duration : </b>Whole fest</p>
+								{/* <p className={styles.event_time}><b>DATE : </b>04.02.2023</p> */}
+								<p className={styles.event_time}><b>CONTACT : </b>Shalini</p>
+							</div>
 					</div>
 				</Container>
 				<Container>

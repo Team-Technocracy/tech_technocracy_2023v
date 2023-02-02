f624dfe2efb0e392ded53219d02a917677e3e905
prem913 <premchandmaddikuntla87@gmail.com>
2023-02-02 14:20:00 +0530

    sidenav onscroll

--- src/components/Sidenav/SideNav.jsx
+++ src/components/Sidenav/SideNav.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useEffect, useRef } from "react";
 import NavLink from "./NavLink";
 import styles from "./navstyles.module.css";
 
@@ -33,8 +35,26 @@ const navList = [
 ];
 
 const SideNav = () => {
+  const [isVisible,setIsVisible] = useState(false)
+  useEffect(()=>{
+    const listenToScroll = () => {
+      let margin = 120;
+      const winScroll = document.body.scrollTop || 
+          document.documentElement.scrollTop;
+      if (winScroll > margin) {  
+           setIsVisible(true);
+      } else {
+           setIsVisible(false);
+      }  
+    };
+  
+    window.addEventListener("scroll",listenToScroll)
+    return ()=>window.removeEventListener("scroll",listenToScroll)
+  },[])
   return (
-    <nav className={styles.nav}>
+    <nav className={styles.nav} style={{
+      opacity:isVisible?1:0
+    }}>
       <ul>
         {navList.map((item, index) => {
           return (

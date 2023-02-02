768b27270222cd6093b328771a5c2860273c1086
Rahul singh <rahulsingh19390@gmail.com>
2023-02-02 16:13:28 +0530

    Added google form For Technomic Quotient

--- src/components/Forms/Registration/Gform.jsx
+++ src/components/Forms/Registration/Gform.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Gform() {
   return (
-    <div>
+    <div style={{background: "linear-gradient(to right, #1e2464, #090a1f)"}}>
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdVe65UmhXjjlxwcibPK7Zm_FGbpgON4p8iwk4DAtobV9O41w/viewform?embedded=true" width="100%" height="1541" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   )

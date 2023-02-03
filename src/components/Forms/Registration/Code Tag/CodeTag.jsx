8cdee87ed95318f38c9d26962a755a0e2e7a4932
Rahul singh <rahulsingh19390@gmail.com>
2023-02-03 12:57:09 +0530

    Updated Codetag Details

--- src/components/Forms/Registration/Code Tag/CodeTag.jsx
+++ src/components/Forms/Registration/Code Tag/CodeTag.jsx
@@ -133,7 +133,7 @@ function CodeTag() {
 									<li>- Any malpractice or illicit activities will lead to immediate disqualification.</li>
 								</ul>
 							</p>
-							<p className={styles.event_team}><b>Requirements: </b>3 Whiteboards markers and dusters. 3-4 Executives for invigilation and setting up the code editor. 3-4 volunteers for managing the oncoming teams. The number of volunteers/executives required can vary with the number of registrations.</p>
+							{/* <p className={styles.event_team}><b>Requirements: </b>3 Whiteboards markers and dusters. 3-4 Executives for invigilation and setting up the code editor. 3-4 volunteers for managing the oncoming teams. The number of volunteers/executives required can vary with the number of registrations.</p> */}
 							<p className={styles.event_procedure}><b>PRIZES : </b>Vouchers</p>
 							<p className={styles.event_location}><b>LOCATION : </b>CCC or Lab</p>
 							<p className={styles.event_time}><b>TIME : </b>9:30 AM- 11 AM</p>

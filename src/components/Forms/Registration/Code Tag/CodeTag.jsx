0fa27b2e8649bfc5decf2f37e20166b8625184d4
Somil Puri <somilpuri07@gmail.com>
2023-02-03 12:04:52 +0530

    Form Alterations

--- src/components/Forms/Registration/Code Tag/CodeTag.jsx
+++ src/components/Forms/Registration/Code Tag/CodeTag.jsx
@@ -4,14 +4,14 @@ import { useState } from "react";
 import styles from "../Styles/styles.module.css";
 import Navbar from '../../../Home/Navbar-new/Navbar'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Container, TextField, Grid, useThemeProps } from "@mui/material";
+import { Container, TextField, Grid, useThemeProps, Alert } from "@mui/material";
 import { useParams } from "react-router-dom";
 // import events from '../../../assets/datas/EventsDatas'
 import axios from "axios";
 // import Cookies from 'js-cookie';
 import { NavLink } from "react-router-dom";
-import img from '../../../../assets/images/leftArrow.png'
-
+import img from '../../../../assets/images/leftArrow.png' 
+import useFullPageLoader from '../../../utils/useFullPageLoader';
 const darkTheme = createTheme({
 	palette: {
 		mode: 'dark',
@@ -19,7 +19,11 @@ const darkTheme = createTheme({
 });
 
 function CodeTag() {
-
+	const [loader,showLoader,hideLoader]= useFullPageLoader();
+	const [alert0, setErrorAlert] = useState(false);
+	const [alert1, setAlert] = useState(false);
+    const [alertContent0, setErrorAlertContent] = useState('');
+	const [alertContent1, setSuccessAlertContent] = useState('');
 	const { id } = useParams();
 	// data of event
 	const data = {
@@ -66,17 +70,30 @@ function CodeTag() {
 	}
 
 	function submit() {
-		console.log(form);
-		alert("Please wait...Don't refresh the page");
-		axios.post(`https://aavartan-backend-production.up.railway.app/codetag/${JSON.stringify(form)}`)
-			.then(res => {
-				if (res.data === 0) {
-					alert("Error occurred");
-				}
-				else if (res.data === 1) {
-					alert("Team registered successfully");
-				}
-			})
+		showLoader();
+		if (form.event !== ""&&form.team_name !== ""&&form.leader_name !== ""&&form.leader_mail !== ""&&form.leader_number !== "" && form.leader_whatsapp !== ""&&form.leader_college !== ""&&form.yos !== ""&&form.leader_branch !== ""&&form.mem3 !== ""&&form.mem2 !== ""&&form.lang !== "") {
+			console.log(form);
+			axios.post(`https://aavartan-backend-production.up.railway.app/codetag/${JSON.stringify(form)}`)
+				.then(res => {
+					if (res.data === 0) {
+						hideLoader();
+						setErrorAlertContent('Error occurred');
+						setErrorAlert(true);
+						setAlert(false);
+					}
+					else if (res.data === 1) {
+						hideLoader();
+						setSuccessAlertContent('Registered successfully');
+						setAlert(true);
+						setErrorAlert(false);
+					}
+				})
+		}else{
+			hideLoader();
+			setErrorAlertContent('Fill the required details!!!');
+			setErrorAlert(true);
+			setAlert(false);
+		}
 	}
 
 	const [attri, setAttri] = useState(false);
@@ -102,11 +119,11 @@ function CodeTag() {
 						<div>
 							<h3 className={styles.event_title}>CODE TAG</h3>
 							<p className={styles.event_description}>Get yourself prepared to scrutinize, perceive, and decode the problem statement from images. Make no provision to compete with the finest intellects of coding.</p>
-							<p className={styles.event_more}><b>DESCRIPTION: </b>A team of 3 will participate in the event and compete with multiple other teams. A coding question will be explained to one of the teammates and they will try to explain the problem statement to their teammate through gestures and illustrations/drawings on a whiteboard/blackboard. The other teammate has to solve the question according to their interpretation of what their teammate is trying to explain. The code will be judged by the coder’s comprehension of the question and the number of test cases passed upon submission. The code will be submitted on reliable online platforms like InterviewBit, SPOJ, CodeStudio, etc.</p>
+							<p className={styles.event_more}><b>DESCRIPTION: </b>A team of 2 will participate in the event and compete with multiple other teams. A coding question will be explained to one of the teammates and they will try to explain the problem statement to their teammate through gestures and illustrations/drawings on a whiteboard/blackboard. The other teammate has to solve the question according to their interpretation of what their teammate is trying to explain. The code will be judged by the coder’s comprehension of the question and the number of test cases passed upon submission. The code will be submitted on reliable online platforms like InterviewBit, SPOJ, CodeStudio, etc.</p>
 							<p className={styles.event_materials}>
 								<b>RULES : </b>
 								<ul>
-									<li>- Each team strictly consists of 3 people.</li>
+									<li>- Each team strictly consists of 2 people.</li>
 									<li>- Participants are free to use any programming language (C, C++, Java, Python etc.).</li>
 									<li>- The question will be based on DSA, and will be explained to only one of the teammates.</li>
 									<li>- A blackboard/whiteboard and a computer system with a code editor will be provided to the participants by the management team.</li>
@@ -118,10 +135,10 @@ function CodeTag() {
 							</p>
 							<p className={styles.event_team}><b>Requirements: </b>3 Whiteboards markers and dusters. 3-4 Executives for invigilation and setting up the code editor. 3-4 volunteers for managing the oncoming teams. The number of volunteers/executives required can vary with the number of registrations.</p>
 							<p className={styles.event_procedure}><b>PRIZES : </b>Vouchers</p>
-							<p className={styles.event_location}><b>LOCATION : </b>CSE lab 2</p>
-							<p className={styles.event_time}><b>TIME : </b>9:30 AM - 11 AM</p>
+							<p className={styles.event_location}><b>LOCATION : </b>CCC or Lab</p>
+							<p className={styles.event_time}><b>TIME : </b>9:30 AM- 11 AM</p>
 							<p className={styles.event_time}><b>DATE : </b>05.02.2023</p>
-							<p className={styles.event_time}><b>CONTACT : </b> Gaurav Kumar Sahu: 9950251125, Rishav Gandhi: 7979059220</p>
+							<p className={styles.event_time}><b>CONTACT : </b>Gaurav : 9950251125, Rishav : 7979059220</p>
 						</div>
 					</div>
 				</Container>
@@ -213,7 +230,6 @@ function CodeTag() {
 											label="Team Leader Contact no"
 											variant="outlined"
 											autoComplete='off'
-											type="number"
 											onKeyUp={(e) => handle(e)}
 										/>
 									</Grid>
@@ -286,6 +302,11 @@ function CodeTag() {
 										/>
 									</Grid>
 								</Grid>
+								<br></br>
+								   {alert0 ? <Alert variant="outlined" severity='error'>{alertContent0}</Alert> : <></> }
+								   <br></br>
+								   {alert1 ? <Alert variant="outlined" severity='success'>{alertContent1}</Alert> : <></> }
+								   <br></br>
 								<button type="button" className={styles.registration_button} onClick={submit} >Register</button>
 							</form>
 						</Formik>
@@ -295,6 +316,7 @@ function CodeTag() {
 
 			</div>
 		</ThemeProvider>
+		{loader}
 		</>
 	);
 }

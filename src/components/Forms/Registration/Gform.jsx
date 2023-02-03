6218f223f7568fc3dd85015453679fd1cf948dc4
Somil Puri <somilpuri07@gmail.com>
2023-02-03 16:44:38 +0530

    route changed

--- src/components/Forms/Registration/Gform.jsx
+++ src/components/Forms/Registration/Gform.jsx
@@ -4,7 +4,9 @@ function Gform() {
   return (
     <div style={{background: "linear-gradient(to right, #1e2464, #090a1f)"}}>
     <div></div>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe4jsiKoE08rJxT_varyC_xYs1CtNCSzJKBfu544HKdJ5xlfA/viewform" width="100%" height="1541" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe4jsiKoE08rJxT_varyC_xYs1CtNCSzJKBfu544HKdJ5xlfA/viewform" width="100%" height="1541" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe4jsiKoE08rJxT_varyC_xYs1CtNCSzJKBfu544HKdJ5xlfA/viewform" width="100%" height="2216" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
     </div>
   )
 }
